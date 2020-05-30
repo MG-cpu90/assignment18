04fdef51690aa246978b865a9e8006a2c7c5e06f
Massimo Guida <massimo@massimoguida.com>
2020-05-30 18:03:31 -0400

    created webpack.config.js file

---
+++ Develop/public/webpack.config.js
@@ -0,0 +1,45 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "development",
+
+  // add entry points for JavaScript files for the three pages, home, detail, and favorites.
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Online/Offline Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An app which allows users to track their budget both online and offline",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("public/icons/icon-192x192.png"),
+        sizes: [192, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
