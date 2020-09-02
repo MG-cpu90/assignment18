b7584a3a874cc5a0255a6ae793fea02685ecdbc4
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 11:11:24 -0400

    added comment to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require modules
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,16 +17,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://budgetuser1:trackerpassword1@ds053597.mlab.com:53597/heroku_xnb5wbqh", {
-  useNewUrlParser: true,  
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4 // Use IPv4, skip trying IPv6
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-  console.log("Server listening on: http://localhost:" + PORT);
+    console.log(`App running on port ${PORT}!`);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
